fd3020eaaaf3657f003b97f6eb11d1939fa616b9
Victor Stone <victor.stone@gmail.com>
2013-10-18 15:13:22 -0400

    Daily build scripts

--- bin/entdeploy/entdeploy.sh
+++ bin/entdeploy/entdeploy.sh
@@ -7,8 +7,8 @@ date >> buildinfo.txt
 git log -n 5 > gitlog
 grep "^[D ].*$" gitlog > buildlog.txt
 
-FTP_SITE=ftp://timbregr@timbregroove.org/apps
-FTP_IPA=ftp://timbregr@timbregroove.org/apps/ipa
+FTP_SITE=ftp://timbregr@timbregroove.org/apps/daily
+FTP_IPA=ftp://timbregr@timbregroove.org/apps/daily/ipa
 
 ftp -u $FTP_SITE/buildinfo.txt buildinfo.txt;type=A
 ftp -u $FTP_SITE/buildlog.txt  buildlog.txt;type=A
